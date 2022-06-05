e557525f3c648b5e9841ffea1f7611c9ea676a50
pntech20 <pntech20@gmail.com>
2022-06-05 15:59:56 +0700

    Init code

---
+++ contract/target/wasm32-unknown-unknown/release/greeter.d
@@ -0,0 +1,1 @@
+/Users/mac/Documents/Learn/NEAR/Blog/contract/target/wasm32-unknown-unknown/release/greeter.wasm: /Users/mac/Documents/Learn/NEAR/Blog/contract/src/lib.rs
---
+++ contract/target/wasm32-unknown-unknown/release/libgreeter.d
@@ -0,0 +1,1 @@
+/Users/mac/Documents/Learn/NEAR/Blog/contract/target/wasm32-unknown-unknown/release/libgreeter.rlib: /Users/mac/Documents/Learn/NEAR/Blog/contract/src/lib.rs
